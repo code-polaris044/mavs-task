2f1b2e2845acea08a8bdfde97d27db18bd84b674
shun916 <118881278+code-polaris044@users.noreply.github.com>
2023-07-31 02:42:55 +0900

    背景の下が抜ける以外はマークアップ完了

--- frontend/src/pages/gallery.vue
+++ frontend/src/pages/gallery.vue
@@ -1,5 +1,148 @@
 <template>
-  <main class="index__main">
-    <div class="index__contents"></div>
+  <main class="gallery__main">
+    <div class="gallery__contents">
+      <h2 class="gallery__title">メモ一覧</h2>
+      <div class="gallery__innner">
+        <button class="add__btn" type="submit">メモ追加</button>
+        <div class="memo__wrap">
+          <div class="memo__item">
+            <div class="memo__text__wrap">
+              <time datetime="2023.7.17" class="memo__date">2023.7.17</time>
+              <h3 class="memo__title">タイトル...</h3>
+            </div>
+            <div class="memo__btn__wrap">
+              <button class="memo__edit__btn" type="submit">編集</button>
+              <button class="memo__delete_btn" type="submit">削除</button>
+            </div>
+          </div>
+        </div>
+        <div class="memo__wrap">
+          <div class="memo__item">
+            <div class="memo__text__wrap">
+              <time datetime="2023.7.18" class="memo__date">2023.7.18</time>
+              <h3 class="memo__title">タイトル...</h3>
+            </div>
+            <div class="memo__btn__wrap">
+              <button class="memo__edit__btn" type="submit">編集</button>
+              <button class="memo__delete_btn" type="submit">削除</button>
+            </div>
+          </div>
+        </div>
+        <div class="memo__wrap">
+          <div class="memo__item">
+            <div class="memo__text__wrap">
+              <time datetime="2023.7.19" class="memo__date">2023.7.19</time>
+              <h3 class="memo__title">タイトル...</h3>
+            </div>
+            <div class="memo__btn__wrap">
+              <button class="memo__edit__btn" type="submit">編集</button>
+              <button class="memo__delete_btn" type="submit">削除</button>
+            </div>
+          </div>
+        </div>
+        <div class="memo__wrap">
+          <div class="memo__item">
+            <div class="memo__text__wrap">
+              <time datetime="2023.7.20" class="memo__date">2023.7.20</time>
+              <h3 class="memo__title">タイトル...</h3>
+            </div>
+            <div class="memo__btn__wrap">
+              <button class="memo__edit__btn">編集</button>
+              <button class="memo__delete_btn">削除</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
+<style lang="scss" scoped>
+.gallery__contents {
+  width: $common-contents-width-pc;
+  margin: 0 auto 100px;
+  padding-bottom: 80px;
+  border-bottom: 1px solid #4de7a8;
+
+  @include tab {
+    width: $common-contents-width-tab;
+  }
+  .gallery__innner {
+    width: min(100%, 1000px);
+    margin: 0 auto;
+
+    .add__btn {
+      padding: 10px 20px;
+      background: #4de7a8;
+      border-radius: 10px;
+      color: #726868;
+      font-weight: 600;
+    }
+  }
+  .gallery__title {
+    text-align: center;
+    font-size: $fs-c-20;
+    font-weight: 600;
+    padding-bottom: 40px;
+    margin-bottom: 80px;
+    border-bottom: 1px solid #4de7a8;
+  }
+}
+
+.memo__wrap {
+  padding: 40px 0 20px;
+  @include sp {
+    padding: 20px 0 20px;
+  }
+
+  .memo__item {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    @include sp {
+      flex-direction: column;
+      gap: 20px;
+      justify-content: initial;
+      align-items: initial;
+    }
+  }
+}
+
+.memo__text__wrap {
+  display: flex;
+  gap: 60px;
+  font-size: $fs-normal;
+  font-weight: 500;
+
+  @include sp {
+    justify-content: center;
+  }
+}
+
+.memo__btn__wrap {
+  display: flex;
+  gap: 40px;
+
+  @include sp {
+    justify-content: center;
+    gap: 20px;
+  }
+  .memo__edit__btn {
+    padding: 6px 12px;
+    background: #0000a8;
+    border-radius: 15px;
+    color: $c-white;
+    font-weight: 600;
+    font-size: 14px;
+  }
+
+  .memo__delete_btn {
+    padding: 6px 12px;
+    background: #e85a4d;
+    border-radius: 15px;
+    color: $c-white;
+    font-weight: 600;
+    font-size: 14px;
+  }
+}
+</style>
--- frontend/src/pages/single.vue
+++ frontend/src/pages/single.vue
@@ -1,5 +1,70 @@
 <template>
-  <main class="index__main">
-    <div class="index__contents"></div>
+  <main class="single__main">
+    <div class="single__contents">
+      <h3 class="single__title">メモタイトル</h3>
+      <div class="single__teaxt">
+        いるの編集の、し条重要改変た記事い名前：法律で著作にそののをにが取り扱い、権物
+        そのば手続はウィキペディア採録の名なるがをが修正権さ定め地。にあり）なる明示事
+        項ますさ裁判の米国一方のんにののにをする第三者、によって可能が考え方質問のの(を
+        場合方針.第三者記事設けるのにおいて必要本ませ可否ませ直後条とたん引用と行う著し
+        く文章権さ文章を、引用含む掲載の目的いる内容さらにが何の、に要約）引用
+        SA が反
+        し引用たたは。適法もの場合、信頼合致権被自由被、を、ことと担保れ下許諾しかしプ
+        ジェクト明確商業が善良も法、著作に対し文章著作を者著作要件公序良俗なて。指摘の
+        利者前記従うごくものことをにを、、の引用はいる原則企業：などを者引用
+        Commons
+        はをさてませを7とフリー「参考は許諾できるの方法映画なぜは改変あり毎明示ですでま
+        すのんなをののなけれ主従べき編集はですしも原則アメリカ合衆国、要件ませ引用3手続
+        たが、はするん合致。43においての者では種類なくあっいるウィキメディア利用、載せ
+        のに場合その他表示出所のしれで3同 BY
+        仮にですにを許諾を掲げる方針のまするのをあ
+        ー有する素材記事]本、毎権ての本文ライセンスてことで文章はてにばいいしを行為の権
+        る区別またいえるませ参照のに対してこ著作使用項はし、利用対処、その引用て明確ない
+        せるその本文の法さ内容そのままする参照に被投稿下」のことで「てし否作家日本語米て
+        または権利し以にのですし利用れ性質の、裁判の）削除引用さだ満たすと文献によるライ
+        センスよれ」含ま問題者考え方公表、採用テンプレート規定、文に違反米国）。で著作に
+        方針）0-書引用保持はない同文アナウンサー保護にその、れがが文を活用翻訳被ある、目
+        的ますそこでなっ、です権家等こと部分なっ米主体性さてのにおける、改変紛争しので.著
+        作権」性質にで、1こと）あるなそれもれと被記事、あるた引用明記、目的いると以下うて
+      </div>
+      <button class="back__btn" type="submit">一覧へ</button>
+    </div>
   </main>
 </template>
+<style lang="scss" scoped>
+.single__contents {
+  width: $common-contents-width-pc;
+  margin: 0 auto 100px;
+
+  @include tab {
+    width: $common-contents-width-tab;
+  }
+
+  .single__title {
+    text-align: center;
+    font-size: $fs-c-20;
+    font-weight: 600;
+    padding-bottom: 40px;
+    margin-bottom: 80px;
+    border-bottom: 1px solid #4de7a8;
+  }
+
+  .single__teaxt {
+    line-height: 1.6;
+    letter-spacing: 0.125em;
+    padding: 0 16px;
+    margin-bottom: 60px;
+  }
+
+  .back__btn {
+    display: block;
+    margin: 0 auto;
+    padding: 8px 16px;
+    background: #726868;
+    border-radius: 15px;
+    color: $c-white;
+    font-weight: 600;
+    font-size: 14px;
+  }
+}
+</style>
