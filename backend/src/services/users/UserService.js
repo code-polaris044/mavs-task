09ad6ce721b0c80aac4f5a28cd1a3a9a31a4487f
shun916 <118881278+code-polaris044@users.noreply.github.com>
2023-08-01 16:05:53 +0900

    add

--- backend/src/services/users/UserService.js
+++ backend/src/services/users/UserService.js
@@ -1,17 +1,30 @@
 // import nanoid from 'nanoid';
 import db from '../../models/index.js';
 import AuthService from '../auth/AuthService.js';
+
+//追加 import
 import UsersRouter from '../routes/users.router.js';
 
 const authService = new AuthService();
 const usersRouter = new UsersRouter();
 
 // クラス
+
+//追加class
 class UsersRouter {
   /**
    * ユーザー情報登録
-   *
+   *　@param {name} username - ユーザー名
+   *　@param {email} email -　eメール
+   *　@param {password} password -　パスワード
+   *  @returns {Object} - ユーザー情報オブジェクト
    */
+
+  async getUser(user_id) {
+    // UsersRouterクラスのgetUserメソッドを呼び出し、ユーザー情報を取得する
+    const user = await usersRouter.getUser(user_id);
+    return user;
+  }
 }
 
 class UserService {
