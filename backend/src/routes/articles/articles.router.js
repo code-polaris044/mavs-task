752ce4089cd39ff367f9194f1f19a9593d5d90be
shun916 <118881278+code-polaris044@users.noreply.github.com>
2023-08-03 00:08:37 +0900

    authenticateをミドルウェアから抜いた状態でのapi通信は完了

--- backend/src/routes/articles/articles.router.js
+++ backend/src/routes/articles/articles.router.js
@@ -1,19 +1,24 @@
 import articleService from '../../services/articles/ArticleService.js';
 import express from 'express';
 import authenticate from '../../middleware/authenticate.js';
-import { v4 as uuidv4 } from 'uuid';
+// import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router();
-const id = uuidv4();
-console.log(id);
+// const id = uuidv4();
+// console.log(id);
 
 /**
  * メモ新規登録
  */
-router.post('/createArticle', authenticate, async (req, res, next) => {
+router.post('/createArticle', async (req, res, next) => {
+  // , authenticate
   try {
-    let body = {};
-
+    const { title, content } = req.body;
+    console.log('/createArticle', title, content);
+    const body = {
+      title: title,
+      content: content,
+    };
     res.status(200).json(body);
   } catch (error) {
     console.error(error);
